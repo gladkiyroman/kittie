79ba99e2585d96b8d4c628bfc22f198514363868
v0ler0 <73056540+v0ler0@users.noreply.github.com>
2021-11-12 23:33:56 +0200

    Fixed with Roma 2

---
+++ Assets/Scripts/MainGame/Roma/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    public GameObject toDestroy;
+    public GameObject WrongPopUp;
+    public bool isPlayerWithinZone = false;
+    Vector2 movement;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (isPlayerWithinZone && Input.GetKeyDown(KeyCode.Return))
+        {
+            toDestroy = Instantiate(WrongPopUp, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+        }
+
+    }
+
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Checking");
+        isPlayerWithinZone = true;
+
+    }
+
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        Destroy(toDestroy);
+  
+            isPlayerWithinZone = false;
+    }
+}
