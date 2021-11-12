d66f43bcc422cb65db0c21f5fad78d32cc675fb2
Roman <37978847+gladkiyroman@users.noreply.github.com>
2021-11-12 15:34:22 +0200

    added popups

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,9 +7,16 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
 
     public Rigidbody2D rb;
-
+    public GameObject toDestroy;
+    public GameObject enterbtn;
+    
     Vector2 movement;
-    // Update is called once per frame
+
+    void Start()
+    {
+        
+    }
+
     void Update()
     {
         movement.x = Input.GetAxisRaw("Horizontal");
@@ -25,5 +32,11 @@ public class PlayerMovement : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collider)
     {
         Debug.Log("Starting minigame!");
+        toDestroy = Instantiate(enterbtn, new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        Destroy(toDestroy);
     }
 }
--- Assets/npc1.cs
+++ Assets/npc1.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class npc1 : MonoBehaviour
 {
+
+
+    public GameObject enterbtn2;
+
+    //private GameObject toDestroy;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -15,4 +21,16 @@ public class npc1 : MonoBehaviour
     {
         
     }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+       // Instantiate(enterbtn2,
+        //    new Vector3(transform.position.x, transform.position.y, transform.position.z), Quaternion.identity);
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+      //  Destroy(enterbtn2);
+    }
+
 }
